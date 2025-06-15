01a1162662434ca5e3fcb969f49214ef7ca981ad
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-06-15 21:06:17 +0545

    Controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <p>Hi, {{$name}}</p>
+    <p>Your age is {{$age}}</p>
     <h1>This is laravel project</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index']);
 
 Route::get('/about', function () {
     return view('about');
 });
-
-
-
